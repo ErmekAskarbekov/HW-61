ef13fe0adf30009394ee9084565f5b482f944d9a
ermek <ermaaskarbekov@gmail.com>
2022-11-25 14:32:01 +0600

    Создал компоненты с которыми буду работать

---
+++ src/CountryInfoApp/CountriesList/CountryItem/CountryItem.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+const CountryItem = () => {
+    return (
+        <div>
+            CountryItem
+        </div>
+    );
+};
+
+export default CountryItem;
---
+++ src/CountryInfoApp/CountryInfoApp.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CountryInfoApp = () => {
+    return (
+        <div>
+            CountryInfoApp
+        </div>
+    );
+};
+
+export default CountryInfoApp;
